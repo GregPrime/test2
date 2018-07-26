90ff575d42aed9f227f8ac363cd6650619e4d67e
GregPrime <yurisuprime@gmail.com>
2018-07-26 15:56:01 +0300

    0.11

--- test2.py
+++ test2.py
@@ -7,11 +7,11 @@ for fname in photos:
 	photos_md5 = photos_md5 + hashlib.md5(fname).hexdigest()
 print photos_md5
 for index in range(len(photos_md5)):
-	if photos_md5[index].count(l) = 1:
+	if photos_md5[index].count(l) == 1:
 		print photos[index]
 		print 'Duplicates : None'
 	else:
 		print 'Duplicates :'
 		for ind in range(len(photos_md5)):
-			if photos_md5[index]=photos_md5[ind]:
+			if photos_md5[index] == photos_md5[ind]:
 				print photos[ind]
