2281cd3a9ecc96c927adcbb67cc6352d9e2ae0cd
GregPrime <yurisuprime@gmail.com>
2018-07-26 15:55:00 +0300

    0.1

--- test2.py
+++ test2.py
@@ -1,8 +1,17 @@
 import os
 import hashlib
+import collections
 photos = os.listdir('photo')
-photos_with_md5 = {}
+photos_md5 = []
 for fname in photos:
-	photos_with_md5[fname] = hashlib.md5(fname).digest())
-print photos_with_md5
-
+	photos_md5 = photos_md5 + hashlib.md5(fname).hexdigest()
+print photos_md5
+for index in range(len(photos_md5)):
+	if photos_md5[index].count(l) = 1:
+		print photos[index]
+		print 'Duplicates : None'
+	else:
+		print 'Duplicates :'
+		for ind in range(len(photos_md5)):
+			if photos_md5[index]=photos_md5[ind]:
+				print photos[ind]
