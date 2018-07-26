ed02dd94c6f4acb37b43c2a88feaeed5c9cd7d3b
GregPrime <yurisuprime@gmail.com>
2018-07-26 16:32:04 +0300

    0.9

--- test2.py
+++ test2.py
@@ -7,7 +7,6 @@ for fname in photos:
 	photos_md5.append([hashlib.md5(fname).hexdigest()])
 number_of_photo = range(len(photos_md5))
 
-print number_of_photo
 
 for i in number_of_photo:
 	dupes = ''
