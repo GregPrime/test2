33ae7ca338e2e519e6b6ab617783b46c2eb54c6a
GregPrime <yurisuprime@gmail.com>
2018-07-26 14:55:24 +0300

    0.02

--- test2.py
+++ test2.py
@@ -4,6 +4,8 @@ def file_as_bytes(file):
     with file:
         return file.read()
 photos = os.listdir()
-photos_with_md5 = [(fname, hashlib.md5(file_as_bytes(open(fname, 'rb'))).digest()) for fname in photos]
+photos_with_md5 = {}
+for fname in photos:
+	photos_with_md5[fname] = hashlib.md5(file_as_bytes(open(fname, 'rb'))).digest())
 print photos_with_md5
 
