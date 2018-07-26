9c2124943d8f12f169687bd9580a1db1147ec73f
GregPrime <yurisuprime@gmail.com>
2018-07-26 14:32:47 +0300

    0.01

--- test2.py
+++ test2.py
@@ -1,1 +1,9 @@
+import os
+import hashlib
+def file_as_bytes(file):
+    with file:
+        return file.read()
+photos = os.listdir()
+photos_with_md5 = [(fname, hashlib.md5(file_as_bytes(open(fname, 'rb'))).digest()) for fname in photos]
+print photos_with_md5
 
