de2beaad31aa38f1b2daf0d0554d251da06a88e1
GregPrime <yurisuprime@gmail.com>
2018-07-26 16:28:38 +0300

    0.3

--- test2.py
+++ test2.py
@@ -5,12 +5,17 @@ photos = os.listdir('photo')
 photos_md5 = []
 for fname in photos:
 	photos_md5.append([hashlib.md5(fname).hexdigest()])
-for index in range(len(photos_md5)):
-	if photos_md5.count(photos_md5[index]) == 1:
-		print photos[index]
-		print 'Duplicates : None'
-	else:
-		print 'Duplicates :'
-		for ind in range(len(photos_md5)):
-			if photos_md5[index] == photos_md5[ind]:
-				print photos[ind]
+number_of_photo = range(len(photos_md5))
+
+print number_of_photo
+
+for i in number_of_photo:
+	dupes = ''
+	for j in number_of_photo:
+		if photos_md5[i] == photos_md5[j]:
+			dupes = dupes + photos[j]
+	if dupes == '':
+		dupes = 'None'
+	print photos[i]
+	print 'Duplicates :' + dupes
+#End
